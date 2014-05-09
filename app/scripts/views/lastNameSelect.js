ae832919ca05050947c5b18427eb6faaa314b605
mynnx <mark.everett@rackspace.com>
2014-05-09 00:18:35 -0700

    Error message on blank last name submit

--- app/scripts/views/lastNameSelect.js
+++ app/scripts/views/lastNameSelect.js
@@ -9,6 +9,11 @@ define([
     'use strict';
 
     var LastNameSelectView = Backbone.View.extend({
+        errorMessages: [
+            'really?  fucking choose a last name already',
+            'is your cat stepping on your laptop?  choose a last name!'
+        ],
+
         events: {
             'submit #lastNameForm': 'nameChosen'
         },
@@ -25,7 +30,15 @@ define([
         nameChosen: function (e) {
             var name = this.$('#lastNameForm :text').val(),
                 errorMessage;
+            if (name === '') {
+                this.$('#lastNameForm').addClass('has-error');
+                errorMessage = this.errorMessages.shift();
+                if (errorMessage) {
+                    this.$('#lastNameForm').append('<p class="error">' + errorMessage + '</p>');
+                }
+            } else {
                 this.trigger('lastNameSelected', name);
+            }
             e.preventDefault();
         }
     });
